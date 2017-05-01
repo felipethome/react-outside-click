1166fcf8e6a2e370aba90d83dd404adf1366a365
Goran Gajic <fero.null@gmail.com>
2017-05-01 11:28:37 +0200

    fix PropTypes and remove createClass

--- src/OutsideClick.js
+++ src/OutsideClick.js
@@ -5,72 +5,31 @@
  * This source code is licensed under the BSD-style license found in the
  * LICENSE file in the root directory of this source tree.
  */
+import PropTypes from 'prop-types';
+import React from 'react';
 
-var React = require('react');
-
-var OutsideClick = React.createClass({
-  displayName: 'OutsideClick',
-
-  propTypes: {
-    /**
-     * The children of this component. They will be put inside of the root
-     * element without any modification.
-     */
-    children: React.PropTypes.any,
-    /**
-     * The component that will wrap the children. Default: "div".
-     */
-    component: React.PropTypes.string,
-    /**
-     * If true, the handler will be removed and outside click will not be
-     * be triggered. Default: false.
-     */
-    disableHandler: React.PropTypes.bool,
-    /**
-     * This is mapped just to avoid ESLint warnings. Any property passed to
-     * this component that is not direct part of its logic will be placed in the
-     * root element without modification.
-     */
-    onClick: React.PropTypes.func,
-    /**
-     * Callback that will be called with the event object when a click happens
-     * outside of the wrap/root element.
-     */
-    onOutsideClick: React.PropTypes.func,
-    /**
-     * If true, the logic of the component will use capture events.
-     * Default: false.
-     */
-    useCapture: React.PropTypes.bool,
-  },
-
-  getDefaultProps: function () {
-    return {
-      component: 'div',
-    };
-  },
-
-  componentWillMount: function () {
+class OutsideClick extends React.Component {
+  constructor(props) {
+    super(props);
+    this._handleDocumentClick = this._handleDocumentClick.bind(this);
+  }
+  componentWillMount() {
     this._wasInside = false;
-  },
-
-  componentDidMount: function () {
+  }
+  componentDidMount() {
     this._addListener(this.props.useCapture);
-  },
-
-  componentWillReceiveProps: function (nextProps) {
+  }
+  componentWillReceiveProps (nextProps) {
     if ((nextProps.disableHandler !== this.props.disableHandler) ||
         (nextProps.useCapture !== this.props.useCapture)) {
       this._removeListener(this.props.useCapture);
       if (!nextProps.disableHandler) this._addListener(nextProps.useCapture);
     }
-  },
-
-  componentWillUnmount: function () {
+  }
+  componentWillUnmount () {
     this._removeListener(this.props.useCapture);
-  },
-
-  _addListener: function (useCapture) {
+  }
+  _addListener (useCapture) {
     if (document) {
       document.addEventListener(
         'click',
@@ -78,9 +37,8 @@ var OutsideClick = React.createClass({
         useCapture
       );
     }
-  },
-
-  _removeListener: function (useCapture) {
+  }
+  _removeListener (useCapture) {
     if (document) {
       document.removeEventListener(
         'click',
@@ -88,60 +46,86 @@ var OutsideClick = React.createClass({
         useCapture
       );
     }
-  },
-
-  _checkForBubblePhase: function (event) {
+  }
+  _checkForBubblePhase (event) {
     if (!this._wasInside) {
       this._handleOutsideClick(event);
     }
     this._wasInside = false;
-  },
-
-  _checkForCapturePhase: function (event) {
+  }
+  _checkForCapturePhase (event) {
     if (this._elem && !this._elem.contains(event.target)) {
       this._handleOutsideClick(event);
     }
-  },
-
-  _handleDocumentClick: function (event) {
+  }
+  _handleDocumentClick (event) {
     if (this.props.useCapture) {
       this._checkForCapturePhase(event);
     }
     else {
       this._checkForBubblePhase(event);
     }
-  },
-
-  _handleOutsideClick: function (event) {
+  }
+  _handleOutsideClick (event) {
     if (this.props.onOutsideClick) this.props.onOutsideClick(event);
-  },
-
-  _handleWrapperClick: function (event) {
+  }
+  _handleWrapperClick (event) {
     this._wasInside = true;
     if (this.props.onClick) this.props.onClick(event);
-  },
+  }
+  render () {
+    const {
+      children,
+      component,
+      onClick,
+      onOutsideClick,
+      useCapture,
+      ...other
+    } = this.props;
 
-  render: function () {
-    var newProps = Object.assign({}, this.props);
-    delete newProps.children;
-    delete newProps.component;
-    delete newProps.onClick;
-    delete newProps.onOutsideClick;
-    delete newProps.useCapture;
+    return React.createElement(component, {
+      ref: (el) => { this._elem = el; },
+      onClick: (e) => this._handleWrapperClick(e),
+      ...other,
+    }, children);
+  }
+};
 
-    newProps.ref = function (elem) {
-      this._elem = elem;
-    }.bind(this);
+OutsideClick.propTypes = {
+  /**
+   * The children of this component. They will be put inside of the root
+   * element without any modification.
+   */
+  children: PropTypes.any,
+  /**
+   * The component that will wrap the children. Default: "div".
+   */
+  component: PropTypes.string,
+  /**
+   * If true, the handler will be removed and outside click will not be
+   * be triggered. Default: false.
+   */
+  disableHandler: PropTypes.bool,
+  /**
+   * This is mapped just to avoid ESLint warnings. Any property passed to
+   * this component that is not direct part of its logic will be placed in the
+   * root element without modification.
+   */
+  onClick: PropTypes.func,
+  /**
+   * Callback that will be called with the event object when a click happens
+   * outside of the wrap/root element.
+   */
+  onOutsideClick: PropTypes.func,
+  /**
+   * If true, the logic of the component will use capture events.
+   * Default: false.
+   */
+  useCapture: PropTypes.bool,
+};
 
-    newProps.onClick = this._handleWrapperClick;
-
-    return React.createElement(
-      this.props.component,
-      newProps,
-      this.props.children
-    );
-  },
-
-});
+OutsideClick.defaultProps = {
+  component: 'div',
+};
 
 module.exports = OutsideClick;
